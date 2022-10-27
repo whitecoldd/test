4e8947eba7236872d5cf0611b54541f4b30a8820
whitecoldd <buyukliwc@gmail.com>
2022-10-27 21:19:48 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useAsyncMemo } from "use-async-memo";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { publicRequest } from "./requestApi";
 function App() {
   const [id, setId] = useState(1);
   const [char, setChar] = useState();
 
-  const newChar = useAsyncMemo(async () => {
-    const res = await publicRequest.get(`/characters/${id}`);
-    setChar(res.data[0]);
-    console.log(char);
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const res = await publicRequest.get(`/characters/${id}`);
+        setChar(res.data[0]);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getItems();
   }, [id]);
-  console.log(newChar);
   return (
     <>
       <input type="number" onChange={(e) => setId(e.target.value)}></input>
